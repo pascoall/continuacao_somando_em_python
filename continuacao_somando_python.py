bc70c1fd9cef5666f9a6c7de23e9dc4b9c123fdd
pascoall <79522993+pascoall@users.noreply.github.com>
2023-10-17 12:31:48 -0300

    continuacao_somando_python.py

--- continuacao_somando_python.py
+++ continuacao_somando_python.py
@@ -1,26 +1,7 @@
-
-
-#y = 2
-#while (y <= 100):
-#    print(y)
-#    y +=4
-
-
-#soma = 0
-#c = 0
-#while(c<=20):
-#    soma = soma + c
-#    c = c + 1
-
-
-c = 0 #valor inicial de c(contador)
-s = 0 #variavel que vai acumular os valores
-while(c <= 100):
-    s += c
+c = 1
+s = 0
+while( c <= 5):
+    r = float(input('Digite um valor: '))
+    s += r
     c += 1
-
 print(s)
-print('Fim do programa')
-    
-    
-   
