a36b62c3c5814abae85873bc47856a5d5f758970
pascoall <79522993+pascoall@users.noreply.github.com>
2023-10-17 12:30:21 -0300

    Add files via upload

---
+++ continuacao_somando_python.py
@@ -0,0 +1,26 @@
+
+
+#y = 2
+#while (y <= 100):
+#    print(y)
+#    y +=4
+
+
+#soma = 0
+#c = 0
+#while(c<=20):
+#    soma = soma + c
+#    c = c + 1
+
+
+c = 0 #valor inicial de c(contador)
+s = 0 #variavel que vai acumular os valores
+while(c <= 100):
+    s += c
+    c += 1
+
+print(s)
+print('Fim do programa')
+    
+    
+   
